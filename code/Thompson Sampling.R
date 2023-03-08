884ff8ae527ea7ef508d2c0c5902314fcca18acb
Yinghui Zhou <102707580+YinghuiZhouu@users.noreply.github.com>
2023-03-08 01:24:54 -0600

    Add data generation function
    Add data generation function; delete config; update idx

--- code/Thompson Sampling.R
+++ code/Thompson Sampling.R
@@ -1,38 +1,25 @@
-library(methods)
-# To use setRefClass(); See http://adv-r.had.co.nz/R5.html. setRefClass() creates "reference classes". In short, reference classes allow you to create objects that behave more like objects in object-oriented programming languages (e.g. Python, Java)
 library(glmnet)
 # Machine learning package, replacing sklearn.linear_model in python
 
-
-LinTSModel <- setRefClass("LinTSModel",
-                          # Linear Thompson Sampling. Computes a different model for each arm.
-                          fields = list(num_mc = "numeric",
-                                        K = "numeric", # Number of arms
-                                        p = "numeric", # Dimension of contexts
-                                        floor_start = "numeric", # Assignment probability floor starting point
-                                        floor_decay = "numeric", # Assignment probability floor decaying rate
-                                        mu = "numeric",
-                                        V = "numeric",
-                                        X = "list",
-                                        y = "list"), 
-                          methods = list(
-                            initialize = function(K, p, floor_start, floor_decay, num_mc = 100) {
-                              self$K <- K
-                              self$p <- p
-                              self$floor_start <- floor_start
-                              self$floor_decay <- floor_decay
-                              self$num_mc <- num_mc
-                              self$mu <- matrix(0, nrow = K, ncol = p + 1)
-                              self$V <- array(0, dim = c(K, p + 1, p + 1))
-                              self$X <- replicate(K, list(), simplify = FALSE)
-                              self$y <- replicate(K, list(), simplify = FALSE)
-                            }))
+LinTSModel <- function(K, p, floor_start, floor_decay, num_mc=100) {
+  model <- list()
+  model$num_mc <- num_mc
+  model$K <- K
+  model$p <- p
+  model$floor_start <- floor_start
+  model$floor_decay <- floor_decay
+  model$mu <- matrix(0, nrow=K, ncol=p+1)
+  model$V <- array(0, dim=c(K, p+1, p+1))
+  model$X <- vector("list", K)
+  model$y <- vector("list", K)
+  return(model)
+}
 
 update_thompson_1 = function(x, y, w, model) {
   # input: 
-  model$X[[w]] <- rbind(X[[w]], x)
-  model$y[[w]] <- rbind(y[[w]], y)
-  regr <- cv.glmnet(x, y, alpha = 0) #(alphas=[1e-5, 1e-4,1e-3, 1e-2, 1e-1, 1.0])?)
+  model$X[[w]] <- rbind(model$X[[w]], x)
+  model$y[[w]] <- rbind(model$y[[w]], y)
+  regr <- cv.glmnet(model$X[[w]], model$y[[w]])             ### ERROR 
   # We'll use this type of lamba value for prediction
   coef <- coef(regr, s = 'lambda.1se') # coefficients
   yhat <- predict(regr, s = 'lambda.1se', newx = x) # prediction
@@ -46,27 +33,27 @@ update_thompson_1 = function(x, y, w, model) {
 
 update_thompson <- function(xs, ws, ys, model) {
   # Updates LinTS agent with newly observed data.
-  # xs: covariate X_t of shape [T, p]
-  # ys: potential outcomes of shape [T, K]
+  # xs: covariate X_t of shape [A, p]
+  # ys: potential outcomes of shape [A, K]
   for (w in 1:model$K) {
-    model <- update_thompson_1(xs[ws == w], ys[ws == w], w, model)
+    model <- update_thompson_1(xs[ws == w], ys[ws == w], w, model) ### DISCUSS: might be something wrong with xs.
   }
   return(model)
 }
 
 draw_thompson <- function(xs, model, start, end, current_t) {
   # Draws arms with a LinTS agent for the observed covariates.
-  T <- dim(xs)[1]
+  A <- dim(xs)[1]
   p <- dim(xs)[2]
-  xt <- cbind(rep(1, T), xs)
+  xt <- cbind(rep(1, A), xs)
   floor <- model$floor_start / current_t^model$floor_decay
   coeff <- array(NA, dim=c(model$K, model$num_mc, p + 1))
   for (w in 1:model$K) {
-    coeff[w,,] <- mvrnorm(model$num_mc, model$mu[w,], model$V[w,,]) # random.multivariate_normal from different contexts?
+    coeff[w,,] <- mvrnorm(model$num_mc, model$mu[w,], model$V[w,,]) # random.multivariate_normal from different contexts
   }
   draws <- t(coeff %*% t(xt))
-  ps <- array(NA, dim=c(T, model$K))
-  for (s in 1:T) {
+  ps <- array(NA, dim=c(A, model$K))
+  for (s in 1:A) {
     ps[s, ] <- table(which.max(draws[, , s], arr.ind=TRUE) - 1) / model$num_mc
     ps[s, ] <- apply_floor(ps[s, ], floor)
   }
--- code/Thompson_experiment.R
+++ code/Thompson_experiment.R
@@ -3,62 +3,100 @@ library(Matrix)
 # Functions
 source('Thompson Sampling.R')
 
-run_experiment <- function(xs, ys, config, batch_sizes) {
+run_experiment <- function(xs, ys, floor_start, floor_decay, batch_sizes) {
   # Run contextual bandit experiment
   # INPUT:
-  # - xs: covariate X_t of shape [T, p]
-  # - ys: potential outcomes of shape [T, K]
-  # - config: dictionary of experiment configs #DISCUSS?
-  # - batch_sizes: nparray of batch sizes ?????
+  # - xs: covariate X_t of shape [A, p]
+  # - ys: potential outcomes of shape [A, K]
   # OUTPUT:
   # - pulled arms, observed rewards, assignment probabilities
-  T <- dim(ys)[1] # T: the number of observations #TODO
+  A <- dim(ys)[1] # A: the number of observations 
   K <- dim(ys)[2] # K: the number of arms
   p <- dim(xs)[2]
-  ws <- numeric(T) # the index of the selected arm? length = T. The ws array is a 1-dimensional array that will store the action
-  yobs <- numeric(T)
-  probs <- matrix(0, T, T, K)
-  Probs_t <- matrix(0, T, K)
-  floor_start <- config$floor_start #DISCUSS?
-  floor_decay <- config$floor_decay
+  ws <- numeric(A) # the index of the selected arm. The ws array is a 1-dimensional array.
+  yobs <- numeric(A)
+  probs <- array(0, dim = c(A, A, K))
+  Probs_t <- matrix(0, A, K)
   
-  if (config$bandit_model == "TSModel") {
-    bandit_model <- LinTSModel(K = K, p = p, floor_start = floor_start, floor_decay = floor_decay)
-    draw_model <- draw_thompson
-  }
+  bandit_model <- LinTSModel(K = K, p = p, floor_start = floor_start, floor_decay = floor_decay)
+  draw_model <- draw_thompson
   
   # uniform sampling at the first batch
-  batch_size_cumsum <- cumsum(batch_sizes) # what is batch_sizes? how to input batch_sizes?
-  ws[1:batch_size_cumsum[1]] <- rep(1:batch_size_cumsum[1] %% K, each = 1)  # batch_size_cumsum[1] = number of obs ? DISCUSS?
-  yobs[1:batch_size_cumsum[1]] <- ys[cbind(1:batch_size_cumsum[1], ws[1:batch_size_cumsum[1]])]
-  probs[1:batch_size_cumsum[1], , ] <- array(1/K, dim = c(batch_size_cumsum[1], T, K))
+  batch_size_cumsum <- cumsum(batch_sizes) # 
+  ws[1:batch_size_cumsum[1]] <- sample(1:batch_size_cumsum[1] %% K)  
+  yobs[1:batch_size_cumsum[1]] <- ys[1:batch_size_cumsum[1], ws[1:batch_size_cumsum[1]]]
+  probs[1:batch_size_cumsum[1], , ] <- array(1/K, dim = c(batch_size_cumsum[1], A, K))
   Probs_t[1:batch_size_cumsum[1], ] <- matrix(1/K, batch_size_cumsum[1], K)
-  if (config$bandit_model == "TSModel") {
-    bandit_model <- update_thompson(xs[1:batch_size_cumsum[1], ], 
+
+  bandit_model <- update_thompson(xs[1:batch_size_cumsum[1], ], 
                                     ws[1:batch_size_cumsum[1]], 
                                     yobs[1:batch_size_cumsum[1]], 
                                     bandit_model)
-  }
   
   # adaptive sampling at the subsequent batches
   for (idx in 1:(length(batch_sizes)-1)) {
-    f <- batch_size_cumsum[idx] 
-    l <- batch_size_cumsum[idx + 1] # is the index identical with python code? DISCUSS?
-    if (config$bandit_model == "TSModel") {
-      draw <- draw_thompson(xs, bandit_model, start=f, end=l, current_t = f)
-    }
+    f <- batch_size_cumsum[idx] + 1
+    l <- batch_size_cumsum[idx + 1] 
+    draw <- draw_thompson(xs, bandit_model, start=f, end=l, current_t = f)
     w <- draw$w
     p <- draw$p
     yobs[f:l] <- ys[cbind(f:l, w)]
     ws[f:l] <- w
-    probs[f:l, , ] <- array(p, dim = c(l - f, T, K))
+    probs[f:l, , ] <- array(p, dim = c(l - f + 1, A, K))
     Probs_t[f:l, ] <- p[f:l, ]
-    if (config$bandit_model == "TSModel") {
-      bandit_model <- update_thompson(
-      xs[f:l], ws[f:l], yobs[f:l], bandit_model)
-    }
+    bandit_model <- update_thompson(
+    xs[f:l], ws[f:l], yobs[f:l], bandit_model)
   }
+  # probs are assignment probabilities e_t(X_s, w) of shape [A, A, K]
   data <- list(yobs = yobs, ws = ws, xs = xs, ys = ys, probs = probs, fitted_bandit_model = bandit_model)
   
   return(data)
 }
+
+generate_bandit_data <- function(n, p, K, noise_std=1.0, signal_strength=1.0) {
+  # Generate covariates and potential outcomes from a classification dataset.
+  # INPUT:
+  # - noise_std: noise standard deviation
+  # - signal_strength: signal strength multiplier
+  # OUTPUT:
+  # - data: covariates and potential outcomes
+  # - mus: arm expected reward over the covariate space
+  
+    X <- matrix(rnorm(n*p), ncol=p) # - X: covariates of shape [A, p]
+    y <- sample(1:K, n, replace=TRUE) # - y: labels of shape [A,]
+    shuffler <- sample.int(n)
+    xs <- X[shuffler, ]
+    ys <- y[shuffler]
+    A <- nrow(xs)
+    A <- min(A, 20000)
+    xs <- xs[1:A, ]
+    ys <- ys[1:A]
+    muxs <- matrix(0, nrow=A, ncol=K)
+    muxs[cbind(1:A, as.numeric(ys))] <- signal_strength
+    ys <- muxs + rnorm(A*K, 0, noise_std)
+    mus <- table(y)/A
+    data <- list(xs=xs, ys=ys, muxs=muxs, A=A, p=ncol(xs), K=K)
+    return(list(data, mus))
+}
+  
+# Set parameters
+floor_start <- 5
+floor_decay <- 0.9
+batch_sizes <- c(100, 200, 300, 400)
+
+# To generate a dataset with 1000 observations, 10 covariates, and 5 arms
+set.seed(123)
+data <- generate_bandit_data(n=1000, p=10, K=5, noise_std=1.0, signal_strength=1.0)
+
+# access dataset components
+xs <- data[[1]]$xs
+ys <- data[[1]]$ys
+muxs <- data[[1]]$muxs
+A <- data[[1]]$A
+p <- data[[1]]$p
+K <- data[[1]]$K
+mus <- data[[2]]
+
+# Run experiment
+results <- run_experiment(xs, ys, floor_start, floor_decay, batch_sizes)
+
