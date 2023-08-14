c110fa45d5500afbfdf4a3aa63091f102109ab22
Amir Williams <amirw0226@gmail.com>
2023-08-14 08:38:05 -0500

    Third commit. Includes updated TS_Experiment_AW.R

--- code/TS_Experiment_AW.R
+++ code/TS_Experiment_AW.R
@@ -3,59 +3,81 @@
 # * Create branch and pull request. .
 # * Make code that works regardless of the sim code.
 
+
+
 # Checks for Thompson Experiment
 
-##### Are batches of correct sizes?
-# Describe code
-batch_splits <- sort(c(cumsum(batch_sizes), cumsum(batch_sizes) +1))
-batch_splits <- batch_splits[batch_splits != 501]
 
-batch_size_check <- results$probs[batch_splits,300,] # Analyze a subset of subjects #write in what we need to see to validate # add columns with the values that I am testing (100, 101, etc.)
-batch_size_check <- as.data.frame(batch_size_check)
-bsc_row_names <- c("Batch 1 End: 100", "Batch 2 Start: 101", "Batch 2 End: 200", "Batch 3 Start: 201", "Batch 3 End: 300", "Batch 4 Start: 301", "Batch 4 End: 400", "Batch 5 Start: 401", "Batch 5 End: 500")
-rownames(batch_size_check) <- bsc_row_names
+##### Code to see if batches are of correct sizes
+
+# Validation: Probabilities will be the same from each batch start to end value, but probabilities will different at each respective batch start.
+
+batch_splits <- sort(c(cumsum(batch_sizes), cumsum(batch_sizes) +1)) # New variable that includes the start and end indexes for batches
+batch_splits <- batch_splits[batch_splits != 501] # Removal of value 501
+
+batch_size_check <- results$probs[batch_splits,300,] # Choose correct subset
+batch_size_check <- as.data.frame(batch_size_check) #make the indexed data a data frame
+
+batch_size_column <- c("Batch 1 End (100)", "Batch 2 Start (101)", "Batch 2 End (200)", "Batch 3 Start (201)", "Batch 3 End (300)", "Batch 4 Start (301)", "Batch 4 End (400)", "Batch 5 Start (401)", "Batch 5 End (500)")
+batch_size_check <- cbind(Batches = batch_size_column, batch_size_check)
+
+bsc_column_Names <- c("Batches (Time Value)", "Treatment 1", "Treatment 2", "Treatment 3", "Treatment 4")
+colnames(batch_size_check) <- bsc_column_Names
+
 batch_size_check
 
+# To view dataframe
+View(batch_size_check)
 
 
 
 ##### In the first batch, are all assignment probabilities uniform? Is this the case across all contexts?
-sum(results$probs[1:batch_sizes[1],,] == 1/data[[1]]$K) # Add in description
+
+# Validation: All probabilities for time 100 for all contexts (A = 500) for all treatments (K = 4) that equal 1/data[[1]]$K will be equivalent to 100 * data[[1]]$A * data[[1]]$K
+
+# This code indexes the first batch of results$probs and makes sure that all values are equal to 0.25
+sum(results$probs[1:batch_sizes[1],,] == 1/data[[1]]$K) # 1/data[[1]]$K is 1/4 = 0.25
 
 
 
-##### Conditional on a context and time period do assignment probabilities always sum to one?
-# Create empty vector with as many elements as observations in the experiment
-sums_to_one <- rep(NA, data[[1]]$A)
+##### Conditional on a context and time period do assignment probabilities always sum to one
+
+# Validation: For every value of time (data[[1]]$A = 500) a value of data[[1]]$A * data[[1]]$K (context * Treatment) will be output
+
+sums_to_one <- rep(NA, data[[1]]$A) # Create empty vector with as many elements as observations in the experiment
 
 # Loop over each time period
 for(i in 1:data[[1]]$A){
   # Save the summed probabilities across treatments for each context conditional
   # On time
-  sums_to_one[i] <- table(rowSums(results$probs[i,,]))
+  sums_to_one[i] <- table(rowSums(results$probs[i,,])) # Iterate data and make a table to correct rounding
 }
 sums_to_one
-
-## Probabiliites not adding up to 1 #This is why table() is used
-# Initially was results$probs[500,,]
-sum(rowSums(results$probs[1:500,,])==1) # = 461
-sum(rowSums(results$probs[1:500,,])!=1) # = 39
-which(rowSums(results$probs[1:500,,])!=1)
 
 
 
 ##### Are probability floors being correctly implemented? Check that there is no probability below the assigned floor.
 ##### Does the smallest probability assigned get smaller over time?
-# For loop iterates each time and makes sure nothing is below floors
+
+# Validation: floor_check will output FALSE for every time (data[[1]]$A) across context (data[[1]]$A) and treatment (data[[1]]$K)
+
+# For loop iterates each time and makes sure no treatment assignment probabilities are below probability floor
 floor_check <- rep(NA, data[[1]]$A) #what to input in this first slot
 
-for (i in 1:data[[1]]$A) {
-  floor_check[i] <- any(results$probs[i, ,] <  (floor_start / (floor_decay * i)))
+probability_floor <- (floor_start / (floor_decay * i)) # Probability floor equation
+
+# Iterate results$probs to check for values less than calculated probability floors
+for (i in 1:data[[1]]$A){
+
+  floor_check[i] <- any(results$probs[i, ,] < probability_floor)
 }
 floor_check
 
 # Identify where probability floor is not being correctly implemented
-which(!floor_check) # Check the probabilities to see if they are below
+which(floor_check)
+
+# How many values where floor check is not being correctly implemented
+sum(which(floor_check))
 
 
 
@@ -63,8 +85,11 @@ which(!floor_check) # Check the probabilities to see if they are below
 ##### Plot the probability that an observation is assigned its true best treatment over time. Is this decreasing?
 ##### Do this separately for each context group, i.e., among observations that arm 1 is best for, among observations that arm 2 is best for, etc.
 
+# Validation: Check that for each "ws <- results$ws[which(data$data$muxs[,1:K]==1)]" the best treatment is assigned to the most subjects
+
+#The code is utilized to analyze how well TS_Experiments adapts treatment assignment to subjects based on the performance of treatments.
 # Assignment of treatment 1
-ws <- results$ws[which(data$data$muxs[,1]==1)] #treatment assigned
+ws <- results$ws[which(data$data$muxs[,1]==1)] # Checks column of muxs for treatment assigned
 A <- length(ws) #number of subjects
 K <- dim(results$probs)[length(dim(results$probs))] #variables, 4 treatments
 dat <- matrix(0, nrow = A, ncol = K)
@@ -97,7 +122,7 @@ dat <- matrix(0, nrow = A, ncol = K)
 dat[cbind(1:A, ws)] <- 1
 dat <- apply(dat, 2, cumsum)
 matplot(dat, type = c("l"), col =1:K,
-        main = "Assignment of Treatment 2",
+        main = "Assignment of Treatment 3",
         xlab = "Subjects (A)",
         ylab = "Number of Subjects Assigned")
 legend("topleft", legend = 1:K, col=1:K, lty=1:K)
